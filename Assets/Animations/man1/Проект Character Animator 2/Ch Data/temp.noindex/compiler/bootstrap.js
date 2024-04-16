0f4e2469dcd231b54a0861d38163197bfa1f1145
SelfKnowledge <s31fknow1edge@gmail.com>
2024-04-16 23:20:23 +0300

    com27
    animTaiga

---
+++ Assets/Animations/man1/Проект Character Animator 2/Ch Data/temp.noindex/compiler/bootstrap.js
@@ -0,0 +1,3 @@
+var __bootStrapPath = _modulePath;
+var __compiledScene = _cwd + "\\scene.json";
+runScript("K:\\Programs\\Adobe_Character_Animator\\Adobe Character Animator 2024\\Support Files\\Runtime\\animal\\scripts\\js\\lib\\engine.js")
