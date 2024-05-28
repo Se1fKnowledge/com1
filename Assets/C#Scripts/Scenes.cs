5d9a94f4fdb20cdc4efcc52c125798c599ff293c
INeksiI <keroninch201@gmail.com>
2024-05-28 15:13:13 +0300

    com45

--- Assets/C#Scripts/Scenes.cs
+++ Assets/C#Scripts/Scenes.cs
@@ -5,12 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Scenes : MonoBehaviour
 {
-    //Смена сцен
     public void ChangeScenes(int numberScenes)
     {
         SceneManager.LoadScene(numberScenes);
     }
-    
     public void Exit()
     {
         Application.Quit();
